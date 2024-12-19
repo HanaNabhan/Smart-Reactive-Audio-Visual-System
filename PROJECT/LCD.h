076e49754587889980dfd0e54c20ace7dffd61df
Hana Nabhan <141133466+HanaNabhan@users.noreply.github.com>
2024-12-19 15:26:26 +0200

    Add files via upload

--- PROJECT/LCD.h
+++ PROJECT/LCD.h
@@ -2,10 +2,14 @@
 #define LCD_H
 
 // Function Prototypes
-void LCD_init(void);
-void LCD_command(unsigned char cmd);
-void LCD_data(char data);
-void LCD_writeString(char* str);
-void delayMs(int delay);
-
+void delayUs(int);   														   //Delay in Micro Seconds
+void delayMs(int);   														   //Delay in Milli Seconds
+void LCD4bits_Init(void);													 //Initialization of LCD Dispaly
+void LCD_Write4bits(unsigned char, unsigned char); //Write data as (4 bits) on LCD
+void LCD_WriteString(char*);											 //Write a string on LCD 
+void LCD4bits_Cmd(unsigned char);									 //Write command 
+void LCD4bits_Data(unsigned char);								 //Write a character
+void LCD_vCLR(void);
+void LCD_vDisplayAtPosition(char row, char column, char *str);
+void LCD_vMoveCursor(char row, char column);
 #endif // LCD_H
