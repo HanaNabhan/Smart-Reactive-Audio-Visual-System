cc708562f492ab399a74557327826951c59880d3
HagarAli <90652610+hagar2@users.noreply.github.com>
2024-12-23 09:47:20 +0200

    Add files via upload

---
+++ PROJECT/ADC.h
@@ -0,0 +1,8 @@
+
+#ifndef ADC_H_
+#define ADC_H_
+// volatile unsigned int ADCvalue; // Digital representation of voltage on PE4
+
+void ADC0_Init_Ch0(void);
+unsigned int ADC0_Read(void);
+#endif /* ADC_H_ */
---
+++ PROJECT/interrupt.h
@@ -0,0 +1,8 @@
+#ifndef INTERRUPT_H
+#define INTERRUPT_H
+
+	void EnableInterrupts(void);
+void INTERRUPT_Init(void);
+int readState1(void);
+int readState2(void);
+#endif
