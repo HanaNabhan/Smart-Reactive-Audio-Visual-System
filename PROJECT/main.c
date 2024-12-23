29c6d617bbf8ed78140b6f4e39c9cafdcc7ff5ac
Hana Nabhan <141133466+HanaNabhan@users.noreply.github.com>
2024-12-23 13:32:35 +0200

    Add files via upload

--- PROJECT/main.c
+++ PROJECT/main.c
@@ -1,62 +1,193 @@
 #include "GPIO.h"
-#include "TM4C123GH6PM.h"
-#include "TExaS.h"
-#include "switch.h"
+#include "LCD.h"
 #include "LED.h"
+#include "switch.h"
+#include "timer.h"
+#include "ADC.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "TExaS.h"
+#define TIMER2_ICR_R            (*((volatile unsigned long *)0x40032024))
+#define SWITCH1_PIN 2 //   pf2(led)
+#define SWITCH2_PIN 5 //(pf1) lcd
+#define LED1_PIN 3//pa6
+#define LED2_PIN 4 //pa3
+#define LED3_PIN 5 //pa4
+#define LED4_PIN 6//pa5
 
-// Global Variables
-void Delay(void);
-void EnableInterrupts(void);
+unsigned int state1 =0, state2=0;
+char flag1 =1 , flag2 =1 ;
+			unsigned int reading=0;
 
-// Function to initialize Port F
-void PortF_Init(void) {
-    GPIO_EnableClock(5);  // Enable clock to Port F
-    GPIO_Init(5, 4, INPUT, DIGITAL);  // Initialize PF4 as input (SW1)
-    GPIO_EnablePullUp(5, 4);  // Enable pull-up resistor on PF4 (SW1)
 
-    // Initialize PF1, PF2, PF3 as outputs (LEDs)
-    GPIO_Init(5, 1, OUTPUT, DIGITAL);  // Initialize PF1 (Red LED)
-    GPIO_Init(5, 2, OUTPUT, DIGITAL);  // Initialize PF2 (Blue LED)
-    GPIO_Init(5, 3, OUTPUT, DIGITAL);  // Initialize PF3 (Green LED)
+void turn_off(void);
+int readAudio() {
+   
+    return ADC0_Read();
 }
 
-// MAIN
-int main(void) {
-    char In;
-    PortF_Init();  // Initialize Port F
-    TExaS_Init(SW_PIN_PF40, LED_PIN_PF321);  // Initialize the TExaS grader for lab 2
-    EnableInterrupts();  // The grader uses interrupts
+
+
+
+
+
+
+void displayOnLCD(unsigned int audio_value) {
+    char buffer[16]; 
+    sprintf(buffer, "Audio: %d", audio_value); 
+LCD_vCLR();	
+    LCD_WriteString(buffer);  
+}
+
+
+void displayOnLED(int audio_value) {
+ 
     
-    // Initialize LEDs and switch
-    BUTTON_voidINIT(5, 4);
-    LED_voidINIT(5, 1);
-    LED_voidINIT(5, 2);
-    LED_voidINIT(5, 3);
+    if (audio_value < 155) {
+        LED_voidON(0,LED1_PIN); 
+        LED_voidOFF(0, LED2_PIN);
+        LED_voidOFF(0, LED3_PIN);
+         LED_voidOFF(0, LED4_PIN);			
+    } else if (audio_value < 160) {
+        LED_voidON(0,LED2_PIN);
+        LED_voidON(0,LED1_PIN);  
+         LED_voidOFF(0, LED3_PIN);
+         LED_voidOFF(0, LED4_PIN);			
+    } else if (audio_value < 4089) {
+        LED_voidON(0,LED3_PIN); 
+        LED_voidON(0,LED2_PIN);
+        LED_voidON(0,LED1_PIN);
+			  LED_voidOFF(0, LED4_PIN);
+    } else {
+        LED_voidON(0,LED4_PIN);
+        LED_voidON(0,LED3_PIN); 
+        LED_voidON(0,LED2_PIN);
+        LED_voidON(0,LED1_PIN);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void turn_off(void)
+{
+	   LED_voidOFF(0,LED1_PIN); 
+        LED_voidOFF(0, LED2_PIN);
+        LED_voidOFF(0, LED3_PIN);
+         LED_voidOFF(0, LED4_PIN);
+}
+ 
+void setup() {
+   
+   BUTTON_voidINIT(2,SWITCH1_PIN);//switch 1 port 5 pin 0
+   BUTTON_voidINIT(2,SWITCH2_PIN);//switch 2 port 5 pin 1
+    LED_voidINIT(0,LED1_PIN);
+	  LED_voidINIT(0,LED2_PIN);
+	  LED_voidINIT(0,LED3_PIN);
+		LED_voidINIT(0,LED4_PIN);
+	   ADC0_Init_Ch0();
+	 // UART0_inti(9600, low, 8, 1, 0, 0);
+	 
+	 // LCD4bits_Init();
+   // LCD_vCLR();
+	//Timer2_Init(16000000*16);
+	
+
+
+
+	
+ 
+  
+
+	
+  
+
+	
+
+	
+    
+}
+
+int main() {
+	TExaS_Init();
+	LCD4bits_Init();
+	LCD_vCLR();
+   BUTTON_voidINIT(0,SWITCH1_PIN);//switch 1 port 5 pin 0
+   BUTTON_voidINIT(2,SWITCH2_PIN);//switch 2 port 5 pin 1*/
+    LED_voidINIT(0,LED1_PIN);
+	  LED_voidINIT(0,LED2_PIN);
+	  LED_voidINIT(0,LED3_PIN);
+		LED_voidINIT(0,LED4_PIN);
+	   ADC0_Init_Ch0();
+	Timer2_Init(80000000);
+   
+	    LCD_WriteString("HELLO");  
+				Timer2_Delay(1);
 
     while (1) {
-        In = BUTTON_charRead(5, 4);  // Read PF4 (SW1) into In using the driver function
-        if (In == 0) {  // Zero means SW1 is pressed
-            LED_voidON(5, 3); // Set PF3 (green LED)
-            LED_voidOFF(5, 1); // Clear PF1 (red LED)
-            LED_voidOFF(5, 2); // Clear PF2 (blue LED)
-        } else {  // 0x10 means SW1 is not pressed
-            LED_voidON(5, 1); // Set PF1 (red LED)
-            LED_voidOFF(5, 3); // Clear PF3 (green LED)
-            LED_voidOFF(5, 2); // Clear PF2 (blue LED)
-        }
-        Delay();  // Wait 0.1 sec
-        LED_voidON(5, 2);  // Set PF2 (blue LED)
-        LED_voidOFF(5, 1);  // Clear PF1 (red LED)
-        LED_voidOFF(5, 3);  // Clear PF3 (green LED)
-        Delay();  // Wait 0.1 sec
-    }
-}
+			reading=ADC0_Read();
+			if((!BUTTON_charRead(0,SWITCH1_PIN)&& flag1)){
+				flag1=0;
+				if(state1==1){
+					state1=0;
+				}
+				else{
+					state1=1;
+				}
+			}
+			flag1=1;
+			if((!BUTTON_charRead(2,SWITCH2_PIN))&& flag2){
+				flag2=0;
+				if(state2==1){
+					state2=0;
+				}
+				else{
+					state2=1;
+				}
+			}
+			flag2=1;
+			//////////////////////////////////
+			if(state1 == 1 && state2 ==1){
+				displayOnLCD(reading);
+				displayOnLED(reading);
+				
+			}
+			else if(state1 == 0 && state2 ==0){
+				LCD_vCLR();
+				turn_off();
+			}
+			
+			else if(state1==1){
+			displayOnLCD(reading);
+				turn_off();
+			}
+			else if(state2==1){
+				displayOnLED(reading);
+							LCD_vCLR();
 
-// Subroutine to wait 0.1 sec
-void Delay(void) {
-    unsigned long volatile time;
-    time = 727240 * 200 / 91;  // 0.1 sec
-    while (time) {
-        time--;
+			}
+			
+			
+			Timer2_Delay(1);
+		
+
     }
 }
